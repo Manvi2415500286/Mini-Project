2e37525b10346b3688a48281bd065e2d94e99aff
unknown <kushwahganesh2006@gmail.com>
2026-04-25 23:43:22 +0530

    update explore.py

--- explore.py
+++ explore.py
@@ -1,8 +1,10 @@
 import pickle
 
-# Load trained model
+
+ # Load trained model
+ 
 with open("student_model.pkl", "rb") as f:
-    model = pickle.load(f)
+     model = pickle.load(f)
 
 print("Enter student details")
 
@@ -12,14 +14,19 @@ absences = float(input("Absences: "))
 G1 = float(input("First Period Grade: "))
 G2 = float(input("Second Period Grade: "))
 
-# Make prediction
+
+ # Make prediction
+
 prediction = model.predict([[studytime, failures, absences, G1, G2]])
 
 grade = prediction[0]
 
 print("\nPredicted Final Grade:", round(grade, 2))
 
-# Intervention logic
+
+
+ # Intervention logic
+
 if grade < 10:
     print("Status: At Risk Student (Needs Intervention)")
 elif grade < 14:
