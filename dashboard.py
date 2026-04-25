bd772714034174ce05ade4479ebd7580826f9832
unknown <kushwahganesh2006@gmail.com>
2026-04-25 23:40:58 +0530

    update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,17 +4,17 @@ import matplotlib.pyplot as plt
 
 data = pd.read_csv("student-mat.csv", sep=";")
 
-# Study time vs grade
+  # Study time vs grade
 sns.scatterplot(x=data["studytime"], y=data["G3"])
 plt.title("Study Time vs Final Grade")
 plt.show()
 
-# Absences vs grade
+  # Absences vs grade
 sns.scatterplot(x=data["absences"], y=data["G3"])
 plt.title("Absences vs Final Grade")
 plt.show()
 
-# Failures distribution
+  # Failures distribution
 sns.countplot(x=data["failures"])
 plt.title("Past Failures Distribution")
 plt.show()
